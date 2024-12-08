01f8163df3d6488a24e87fb4b24e3a51372e4955
djordje.grubac <grubacdjordje@gmail.com>
2024-12-08 20:24:18 +0100

    Second Initial commit

--- InventoryManagement.API/Program.cs
+++ InventoryManagement.API/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
+// builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 
 builder.Services.AddMediatR(configuration =>
